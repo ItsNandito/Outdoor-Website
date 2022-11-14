bf66f3d883bb3290c9eba6690acc0f5fd6b55bea
ItsNandito <fernando.e.criollo@gmail.com>
2022-11-14 12:31:05 -0500

    script to auto fill dropdowns with corresponding data

---
+++ scripts/searchEngine.js
@@ -0,0 +1,31 @@
+//importing the arrays from other JS files
+
+import {nationalParksArray} from './nationalParkData.js';
+import {parkTypesArray} from "./parkTypeData.js";
+import {locationsArray} from "./locationData.js";
+
+
+// Need to fill dropdowns with park type and locations information
+
+//getting the length of the location array & Park Type
+let locationArrayLength = locationsArray.length;
+let parkTypesArrayLength = parkTypesArray.length;
+
+
+//for loop to fill in the drop down with its corresponding properties
+for (let i = 0; i < locationArrayLength; i++) {
+    let newOption = new Option(locationsArray[i], locationsArray[i])
+    const selectState = document.getElementById('stateDropDown'); 
+    selectState.add(newOption,undefined);
+}
+
+//for loop to fill in the drop down with its corresponding properties
+for (let i = 0; i < parkTypesArrayLength; i++) {
+    let newOption = new Option(parkTypesArray[i], parkTypesArray[i])
+    const selectParkType = document.getElementById('parkType'); 
+    selectParkType.add(newOption,undefined);
+}
+
+
+
+//upon clicking search, it will retain the selected dropdown items and use that to filter array
