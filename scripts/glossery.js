6c0575bf61bf2c5f79727468ed332e23eb579983
ItsNandito <fernando.e.criollo@gmail.com>
2022-11-16 11:55:18 -0500

    drop down auto fills with pre givin data and store that data for future use

--- scripts/glossery.js
+++ scripts/glossery.js
@@ -7,3 +7,30 @@ for (let i = 0; i < mountainsArrayLength; i++) {
     const selectMountain = document.getElementById("mountainSelection");
     selectMountain.add(newOption, undefined);
   }
+
+//create a variable that will push the selected item to our next function
+let dropDown = document.getElementById('mountainSelection');
+
+dropDown.onchange = () => {
+    let dropDownValue = dropDown.value;
+    console.log(dropDownValue);
+    arraySelector(dropDownValue)
+    //cardUpdater(mountain);
+};
+
+//perhaps use indexof to locate the name name and what index its in and push that to our cardudpater function
+function arraySelector(mountain) {
+    let arrayindex = mountainsArray.findIndex(array => {
+        //console.log(array.name == mountain);
+        return array.name == mountain
+    })
+
+    console.log(arrayindex)
+
+
+}
+
+function cardUpdater(mountain) {
+    let canvas =  document.getElementById('canvas');
+    let mtnName = mountainsArray.name
+}
